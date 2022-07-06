ac4fbbc2812b7fff6576de0f514b764d465cab7a
Justin Deng <jden@odoo.com>
2022-07-06 10:28:50 -0400

    [ADD] Boilerplate module code

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Data Cleaning Enhacement Module',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'author': 'Odoo Inc',
+    'category': '',
+    'summary': 'Add additional functionaliy to the data cleaning module',
+    'description': """
+    """,
+    'depends': ['base'],
+}
