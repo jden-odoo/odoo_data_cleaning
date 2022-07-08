324eaf5bb45f359f351c3f718c8a7882fe791edc
Justin Deng <jden@odoo.com>
2022-07-08 14:35:59 -0400

    working dirty to attribute val.py

--- scripts/combine.py
+++ scripts/combine.py
@@ -9,13 +9,41 @@ import pandas as pd
 #############################################################################
 
 def main():
+    print('main called')
     attr_val_dict = create_attr_val_dict()
-    final_data_df = add_attr_to_clean(attr_val_dict)
-    final_data_df.to_excel(excel_writer = '../data/final_output.xlsx')
+    print('main finished')
+    #print(attr_val_dict['manufacturer'])
+    # for row in attr_val_dict.keys():
+    #     print(attr_val_dict[row])
+    #     break
+
+    #final_data_df = add_attr_to_clean(attr_val_dict)
+
+    #final_data_df.to_excel(excel_writer = '../data/final_output.xlsx')
 
 #############################################
 # Convert attribute/value data to a dictionary
-# Input: Datafram from excel file
+# Input: None. The name of the file to be opened is hard coded.
+# Output: Nested dictionary, where outer key is attribute name and inner key is value name.
+# The outer key corresponds to a diciontary with two values inside it
+# 1. 'category-external-id', which is the attributes external id as a string.
+# 2. an inner key, which corresponds to an inner dictionary
+#
+# The inner key's dictionary has the value external ids in it
+# All keys in the dictionary have spaces in them replaced with _
+# External id's are not modified at all
+#
+# Example 
+# Original excel file
+# Attribute_Name,Attribute_External_ID,Value_Name,Value_External_ID
+# Color of Car Body,car_body_color,White,car_body_white
+# ,,Green,car_body_green
+# Color of Car Trim,car_trim_color,White,car_trim_white
+# ,,Green, car_trim_green
+#
+# create_attr_val_dict() will return:#############################################
+# Convert attribute/value data to a dictionary
+# Input: None. The name of the file to be opened is hard coded.
 # Output: Nested dictionary, where outer key is attribute name and inner key is value name.
 # The outer key corresponds to a diciontary with two values inside it
 # 1. 'category-external-id', which is the attributes external id as a string.
@@ -45,25 +73,19 @@ def main():
 #     'color_of_car_trim', {
 #         'car_trim_color_value': {
 #             'category_external_id': 'car_body_color',
-#             'white': 'car_body_white',
-#             'green': 'car_body_green'
+#             'white': 'car_trim_white',
+#             'green': 'car_trim_green'
 #         }
 #     }
 # }
+##################################################
 
 def create_attr_val_dict():
-
-    
-
-    #dirty_to_attribute_values output
-    #attr_val_df = pd.read_excel('../data/attr-val.xlsx')
-
+    print('func called')
 
     #Testing
     attr_val_df = pd.read_excel('../data/original-attr-val.xlsx')
 
-    # attr_name_values = attr_val_df['value_ids/name'] 
-    # attr_name_external_ids = attr_val_df['value_ids/id']
     attr_val_dict = {}
     currDict = {}
     currCategory = None
@@ -76,12 +98,23 @@ def create_attr_val_dict():
             currDict = {}
             currDict['category_external_id'] = str(attr_val_df['id'][row]).lower() 
 
-        currDict[str(attr_val_df['value_ids/name'][row]).lower()] = str(attr_val_df['value_ids/id'][row])
+        newValue = str(attr_val_df['value_ids/name'][row]).replace(' ','_').lower()
+        print(newValue)
+        currDict[newValue] = str(attr_val_df['value_ids/id'][row])
 
     if currCategory:
         attr_val_dict[currCategory] = currDict
 
     return attr_val_dict
+    
+
+############################################################
+# Input: nested dictionary with external ids. See create_attr_val_dict() documentation for details
+# Output: dataframe with two addtional columns, attribute_external_id and value_external_id
+#
+# Adds to empty columns to dataframe, and then loops through every row to populate values in the new columns
+# Values are obtained from the nested dictionary passed as a paramter
+############################################################
 
 
 def add_attr_to_clean(attr_val_dict):
--- scripts/dirty_to_attribute_values.py
+++ scripts/dirty_to_attribute_values.py
@@ -6,7 +6,7 @@
 #   3. company name
 
 #Output: attr-val.xlsx
-#Sample Input: python3 dirty_to_attribute_values.py dirtydata.csv b,c,e,f,g,h,i,j,k companyname
+#Sample Input: python3 dirty_to_attribute_values.py dirtydata.csv b,c,e,f,g,h,i,j,k companyname yard
 
 
 import pandas as pd
@@ -16,53 +16,43 @@ import sys
 display_type = "Radio"
 create_variant = "Instantly"
 visibility = "Visible"
-company_name = "ABA company"
-attributes = ["Name", "Manufacturer", "Collection", "Color", "Vendor_SKU", "Designer", "Fabric_Type", "Fiber_Contents", "Fabric_Width", "Putup_Format", "Sales Price", "Product Category"]
 
 
 # Gets dirty data from dirty data csv
 def get_dirty_data(dirtydata):
     return pd.read_csv(dirtydata)
 
+
 # Returns list of column names ordered by dirty data csv
-def list_names(dirty_data):
+def list_all_columns(dirty_data):
     return dirty_data.columns
 
 
 # Get input names (case insensisitive)
 # Future implementation will be drop down XML
-def get_names(values):
+def get_attribute_names(input_array, all_columns):
     array = []
-    for value in values:
+    for value in input_array:
         value = ord(value.lower())-97
-        array.append(attributes[value])
-    return array
-
-
-#letter array to indexes
-def get_number_columns(values):
-    array = []
-    for value in values:
-        value = ord(value.lower())-97
-        array.append(value)
+        array.append(all_columns[value])
     return array
 
 
 # Create ids for name_list
-def get_ids(name_list, company_name):
+def get_external_ids(name_list):
     ids_list = []
     for cell in name_list:
-        if cell in ids_list and pd.isna(cell):
+        if cell in ids_list and cell != " ":
             ids_list.append(" ")
         else:
-            ids_list.append(company_name.lower()[:2] + "-" + cell.lower())
+            ids_list.append("attribute" + "_" + cell.lower())
     return ids_list
 
 
 # Get value_id/name from dirty data
 def get_value_id_name(column_name, dirty_data):
     value_id_names = []
-    for value in dirty_data.iloc[:,column_name].tolist():
+    for value in dirty_data[column_name].tolist():
         if value not in value_id_names:
             value_id_names.append(value)
     return value_id_names
@@ -76,15 +66,19 @@ def create_csv(df):
 # Create the data to be added to the new dataframe for the output csv
 # Creates value_id/id
 # Appends values into the data and returns a dataframe with data
-def create(input_array, dirtydata, companyname):
+def create(input_array, dirtydata):
+    #creates dirty data dataframe
     dirty_data = get_dirty_data(dirtydata)
-    names_list = get_names(input_array)
-    numbers = get_number_columns(input_array)
-    ids_list = get_ids(names_list, companyname)
+    #all columns of the dirty_data dataframe
+    all_columns = list_all_columns(dirty_data)
+    #get all attribute names from all_columns
+    attribute_names = get_attribute_names(input_array, all_columns)
+    #create ids from names_list
+    external_ids = get_external_ids(attribute_names)
     data = []
     count = 0
-    for name in names_list:
-        value_id_names = get_value_id_name(numbers[count], dirty_data)
+    for name in attribute_names:
+        value_id_names = get_value_id_name(name, dirty_data)
         id_num = 1
         prev = ""
         for value in value_id_names:
@@ -95,18 +89,23 @@ def create(input_array, dirtydata, companyname):
             if prev == name:
                 data.append(["","","","","",value,value_id_id])
             else:
-                data.append([name, ids_list[count], display_type, create_variant, visibility, value, value_id_id.lower()])
+                data.append([name, external_ids[count], display_type, create_variant, visibility, value, value_id_id.lower()])
             prev = name
         count = count + 1
     df = pd.DataFrame(data, columns=["name", "id", "display_type", "create_variant", "visibility", "value_ids/name", "value_ids/id"]).set_index('name')
     return df
 
 
-# Creates csv for dataframesantee
-def parse(input_array, dirtydata, companyname):
-    df = create(input_array, dirtydata, companyname)
+# Creates csv for dataframe
+def parse(input_array, dirtydata):
+    df = create(input_array, dirtydata)
     create_csv(df)
+
 
 arr = sys.argv[2].split(",")
 arr = arr[1:len(arr)-3]
-parse(arr, sys.argv[1], sys.argv[3])
+parse(arr, sys.argv[1])
+
+#arr = sys.argv[3].split(",")
+
+#parse(arr, sys.argv[1])
