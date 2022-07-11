395f6ed5eba47a4bc1348d2ef1f31b1d122ad47f
wiwu-odoo <wiwu@odoo.com>
2022-07-11 15:06:37 -0400

    working on this comp lol

--- scripts/dirty_to_attribute_values.py
+++ scripts/dirty_to_attribute_values.py
@@ -52,8 +52,7 @@ def get_value_id_name(column_name, dirty_data):
     value_id_names = []
     for value in dirty_data[column_name].tolist():
         if value not in value_id_names:
-
-            value_id_names.append(str(value).replace(' ','_').lower())
+            value_id_names.append(value)
     return value_id_names
 
 
